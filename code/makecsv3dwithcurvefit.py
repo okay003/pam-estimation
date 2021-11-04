8cd83b1458ba14699e79e6e3dc4605d49b0aa7ed
okay003 <74394828+okay003@users.noreply.github.com>
2021-11-04 20:33:43 +0900

    .py

--- code/makecsv3dwithcurvefit.py
+++ code/makecsv3dwithcurvefit.py
@@ -25,27 +25,27 @@ def getCsvAsList(rawFileName):
         halfPoint = i
         initPoint = halfPoint-stepNum
 
-  # extruct load, pressure, en/unpress, cast str to float
+  # extruct load, pressure, en/depress, cast str to float
   load = []
   pressure = []
   enpress = []
-  unpress = []
+  depress = []
   for i in range(initPoint,halfPoint+1):
     load.append(int(raw[i][0]))
     pressure.append(int(raw[i][1]))
     enpress.append(float(raw[i][2]))
-    unpress.append(float(raw[len(raw)-i+3][2]))
+    depress.append(float(raw[len(raw)-i+3][2]))
 
   # calc contraction ratio
   relativeZero = enpress[0]
   for i in range(len(enpress)):
     enpress[i] = (enpress[i]-relativeZero)/freeLength*100
-    unpress[i] = (unpress[i]-relativeZero)/freeLength*100
+    depress[i] = (depress[i]-relativeZero)/freeLength*100
 
   # return floated list
   enpressList = [list(map(float,load)),list(map(float,pressure)),list(map(float,enpress))]
-  unpressList = [list(map(float,load)),list(map(float,pressure)),list(map(float,unpress))]
-  return enpressList,unpressList
+  depressList = [list(map(float,load)),list(map(float,pressure)),list(map(float,depress))]
+  return enpressList,depressList
 
 # ---- ---- ---- ---- ---- ---- ---- ----
 
@@ -72,26 +72,26 @@ if rawFiles == []:
 
 # get 3-nested list, 1st: each file, 2nd: load-pressure-contraction, 3rd: each float value
 enpresss = []
-unpresss = []
+depresss = []
 for fileName in rawFiles:
-  en,un = getCsvAsList(fileName)
+  en,de = getCsvAsList(fileName)
   enpresss.append(en)
-  unpresss.append(un)
+  depresss.append(de)
 
 # make pandas.DataFrame, see : https://note.nkmk.me/python-pandas-dataframe-values-columns-index/
 load = []
 pressure = list(map(lambda x: x*0.7/4095, enpresss[0][1])) # old was : pressure = enpress[0][1]
 encon = []
-uncon = []
+decon = []
 for i in range(len(enpresss)):
   load.append((lambda x: x*0.001*9.798406)(enpresss[i][0][0]))
   encon.append(enpresss[i][2])
-  uncon.append(unpresss[i][2])
+  decon.append(depresss[i][2])
 
 endf = pd.DataFrame(encon,columns=pressure,index=load)
-undf = pd.DataFrame(uncon,columns=pressure,index=load)
+dedf = pd.DataFrame(decon,columns=pressure,index=load)
 endf = endf.sort_index()
-undf = undf.sort_index()
+dedf = dedf.sort_index()
 
 # approximate raw data to sigmoid
 # see : https://qiita.com/Hiko630/items/7d9f683276f63270a052
@@ -102,42 +102,42 @@ enys =endf.values # input y : raw contraction
 enconsFitted = [] # output y : Fitted contraction
 enopts = [] # opt : paras of moddedSigmoid-fitting
 ensds = [] # sd : standard deviation from covariance of input-output (maybe)
-unx = undf.columns.values
-unys =undf.values
-unconsFitted = []
-unopts = [] # opt : paras of moddedSigmoid-fitting
-unsds = [] # sd : standard deviation from covariance of input-output (maybe)
+dex = dedf.columns.values
+deys =dedf.values
+deconsFitted = []
+deopts = [] # opt : paras of moddedSigmoid-fitting
+desds = [] # sd : standard deviation from covariance of input-output (maybe)
 for i in range(len(enys)):
     enopt,encov = sciopt.curve_fit(moddedSigmoid,enx,enys[i],p0=(30,20,-0.2))
     enopts.append(enopt.tolist())
     ensds.append(np.sqrt(np.diag(encov)).tolist())
-    unopt,uncov = sciopt.curve_fit(moddedSigmoid,unx,unys[i],p0=(30,20,-0.2))
-    unopts.append(unopt.tolist())
-    unsds.append(np.sqrt(np.diag(uncov)).tolist())
-    encon,uncon = [],[]
+    deopt,decov = sciopt.curve_fit(moddedSigmoid,dex,deys[i],p0=(30,20,-0.2))
+    deopts.append(deopt.tolist())
+    desds.append(np.sqrt(np.diag(decov)).tolist())
+    encon,decon = [],[]
     for x in enx:
         encon.append(moddedSigmoid(x,enopt[0],enopt[1],enopt[2]))
-        uncon.append(moddedSigmoid(x,unopt[0],unopt[1],unopt[2]))
+        decon.append(moddedSigmoid(x,deopt[0],deopt[1],deopt[2]))
     enconsFitted.append(encon)
-    unconsFitted.append(uncon)
+    deconsFitted.append(decon)
 
 # save data as pandas.DataFrame
 endfFitted = pd.DataFrame(enconsFitted,columns=enx,index=endf.index.values)
-undfFitted = pd.DataFrame(unconsFitted,columns=unx,index=undf.index.values)
+dedfFitted = pd.DataFrame(deconsFitted,columns=dex,index=dedf.index.values)
 endfopt = pd.DataFrame(enopts,columns=["opt-a","opt-b","opt-c"],index=endf.index.values)
 endfsd = pd.DataFrame(ensds,columns=["sd-a","sd-b","sd-c"],index=endf.index.values)
-undfopt = pd.DataFrame(unopts,columns=endfopt.columns.values,index=endf.index.values)
-undfsd = pd.DataFrame(unsds,columns=endfsd.columns.values,index=endf.index.values)
+dedfopt = pd.DataFrame(deopts,columns=endfopt.columns.values,index=endf.index.values)
+dedfsd = pd.DataFrame(desds,columns=endfsd.columns.values,index=endf.index.values)
 endfopt = endfopt.join(endfsd)
-undfopt = undfopt.join(undfsd)
+dedfopt = dedfopt.join(dedfsd)
 endfFitted = endfFitted.join(endfopt)
-undfFitted = undfFitted.join(undfopt)
+dedfFitted = dedfFitted.join(dedfopt)
 
 serialNumber = rawFiles[0].split("= (")[1].split(",")[0]
 endf.to_csv("_" + serialNumber + "_en_raw.csv")
-undf.to_csv("_" + serialNumber + "_un_raw.csv")
+dedf.to_csv("_" + serialNumber + "_de_raw.csv")
 endfFitted.to_csv("_" + serialNumber + "_en_fit.csv")
-undfFitted.to_csv("_" + serialNumber + "_un_fit.csv")
+dedfFitted.to_csv("_" + serialNumber + "_de_fit.csv")
 
 # finalize message
 print("\033[36mCompleted successfully, Exit \"" + os.path.basename(__file__) + "\"\033[0m")
--- code/plot3d.py
+++ code/plot3d.py
@@ -7,29 +7,29 @@ import matplotlib.pyplot as plt
 allFiles = os.listdir(os.getcwd())
 enRawName = ""
 enFitName = ""
-unRawName = ""
-unFitName = ""
+deRawName = ""
+deFitName = ""
 for f in allFiles:
   if os.path.isfile(f) and f.startswith("_") and f.endswith(".csv"):
     if f.endswith("_en_raw.csv"):
       enRawName = f
     if f.endswith("_en_fit.csv"):
       enFitName = f
-    if f.endswith("_un_raw.csv"):
-      unRawName = f
-    if f.endswith("_un_fit.csv"):
-      unFitName = f
-if enRawName == "" or enFitName == "" or unRawName == "" or unFitName == "":
+    if f.endswith("_de_raw.csv"):
+      deRawName = f
+    if f.endswith("_de_fit.csv"):
+      deFitName = f
+if enRawName == "" or enFitName == "" or deRawName == "" or deFitName == "":
   print("There is no DataFramed .csv files, Exit program")
   exit()
 endfRaw = pd.read_csv(enRawName,index_col=0)
-undfRaw = pd.read_csv(unRawName,index_col=0)
+dedfRaw = pd.read_csv(deRawName,index_col=0)
 endfFit = pd.read_csv(enFitName,index_col=0)
-undfFit = pd.read_csv(unFitName,index_col=0)
+dedfFit = pd.read_csv(deFitName,index_col=0)
 endfFitVal = endfFit.loc[:,"0.0":"0.7"] # https://www.self-study-blog.com/dokugaku/pandas-dataframe-loc-iloc-at-iat/
 endfFitOSD = endfFit[["opt-a","opt-b","opt-c","sd-a","sd-b","sd-c"]]
-undfFitVal = undfFit.loc[:,"0.0":"0.7"] # https://www.self-study-blog.com/dokugaku/pandas-dataframe-loc-iloc-at-iat/
-undfFitOSD = undfFit[["opt-a","opt-b","opt-c","sd-a","sd-b","sd-c"]]
+dedfFitVal = dedfFit.loc[:,"0.0":"0.7"] # https://www.self-study-blog.com/dokugaku/pandas-dataframe-loc-iloc-at-iat/
+dedfFitOSD = dedfFit[["opt-a","opt-b","opt-c","sd-a","sd-b","sd-c"]]
 
 # preparation for plot
 fig = plt.figure()
@@ -42,47 +42,47 @@ enax = fig.add_subplot(1,3,2,projection="3d",proj_type="ortho") # https://stacko
 enax.set_xlabel("Pressure [MPa]")
 enax.set_ylabel("Load [N]")
 enax.set_zlabel("Contraction rate [%]")
-unax = fig.add_subplot(1,3,3,projection="3d",proj_type="ortho") # https://stackoverflow.com/questions/23840756/
-unax.set_xlabel("Pressure [MPa]")
-unax.set_ylabel("Load [N]")
-unax.set_zlabel("Contraction rate [%]")
+deax = fig.add_subplot(1,3,3,projection="3d",proj_type="ortho") # https://stackoverflow.com/questions/23840756/
+deax.set_xlabel("Pressure [MPa]")
+deax.set_ylabel("Load [N]")
+deax.set_zlabel("Contraction rate [%]")
 euax.view_init(elev=30,azim=-135)
 enax.view_init(elev=30,azim=-135)
-unax.view_init(elev=30,azim=-135)
+deax.view_init(elev=30,azim=-135)
 
-# enun in same plot
+# ende in same plot
 plotx,ploty = np.meshgrid(endfFitVal.columns.values.astype(float),endfFitVal.index.values.astype(float))
 enz = endfFitVal.values.astype(float)
-unz = undfFitVal.values.astype(float)
+dez = dedfFitVal.values.astype(float)
 euax.plot_surface(plotx,ploty,enz,color=(1,0,0,0.16))
 euax.plot_wireframe(plotx,ploty,enz,color=(1,0,0,0.36),label="apprx. to sigmoid (enpressurise)")
-euax.plot_surface(plotx,ploty,unz,color=(0,0,1,0.16))
-euax.plot_wireframe(plotx,ploty,unz,color=(0,0,1,0.36),label="apprx. to sigmoid (depressurise)")
+euax.plot_surface(plotx,ploty,dez,color=(0,0,1,0.16))
+euax.plot_wireframe(plotx,ploty,dez,color=(0,0,1,0.36),label="apprx. to sigmoid (depressurise)")
 plotx,ploty = plotx.flatten(),ploty.flatten()
 enz = endfRaw.values.astype(float).flatten()
-unz = undfRaw.values.astype(float).flatten()
+dez = dedfRaw.values.astype(float).flatten()
 euax.scatter(plotx,ploty,enz,color="indianred",label="raw data (enpressurise)")
-euax.scatter(plotx,ploty,unz,color="steelblue",label="raw data (depressurise)")
+euax.scatter(plotx,ploty,dez,color="steelblue",label="raw data (depressurise)")
 
 # plot fit data, see : https://ja.stackoverflow.com/q/38367
 plotx,ploty = np.meshgrid(endfFitVal.columns.values.astype(float),endfFitVal.index.values.astype(float))
 enz = endfFitVal.values.astype(float)
-unz = undfFitVal.values.astype(float)
+dez = dedfFitVal.values.astype(float)
 enax.plot_surface(plotx,ploty,enz,color=(1,0,0,0.16))
 enax.plot_wireframe(plotx,ploty,enz,color=(1,0,0,0.36))
-unax.plot_surface(plotx,ploty,unz,color=(0,0,1,0.16))
-unax.plot_wireframe(plotx,ploty,unz,color=(0,0,1,0.36))
+deax.plot_surface(plotx,ploty,dez,color=(0,0,1,0.16))
+deax.plot_wireframe(plotx,ploty,dez,color=(0,0,1,0.36))
 
 # plot raw data as bar-scatter, see : https://ja.stackoverflow.com/q/38367
 plotx,ploty = plotx.flatten(),ploty.flatten()
 enz = endfRaw.values.astype(float).flatten()
-unz = undfRaw.values.astype(float).flatten()
+dez = dedfRaw.values.astype(float).flatten()
 # bottom = np.zeros(len(plotx))
 # width = depth = 0.001
 # enax.bar3d(plotx,ploty,bottom,width,depth,enz,color="indianred")
 enax.scatter(plotx,ploty,enz,color="indianred")
-# unax.bar3d(plotx,ploty,bottom,width,depth,unz,color="steelblue")
-unax.scatter(plotx,ploty,unz,color="steelblue")
+# deax.bar3d(plotx,ploty,bottom,width,depth,dez,color="steelblue")
+deax.scatter(plotx,ploty,dez,color="steelblue")
 
 euax.legend(ncol=2,loc=(0.5,1))
 plt.show()
